e5df0b38b6d7daed362d0558b09119158387a90d
fengyang <hackzfy@foxmail.com>
2019-12-05 02:22:37 +0800

    fix: add class 'full-width' to FormlyInputComponent

--- projects/project-shared/formly-shared/component/formly-input/formly-input.component.ts
+++ projects/project-shared/formly-shared/component/formly-input/formly-input.component.ts
@@ -3,7 +3,8 @@ import { FieldType } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-formly-input',
-  templateUrl: 'formly-input.component.html'
+  templateUrl: 'formly-input.component.html',
+  styleUrls: ['./formly-input.component.scss']
 })
 
 export class FormlyInputComponent extends FieldType {
